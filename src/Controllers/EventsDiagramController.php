02b520738d5999242e7a82d71b38e4a3240b3e5f
mnikoei <mohsen.nikoei@gmail.com>
2020-07-07 01:30:21 +0430

    data transform for diagram

--- src/Controllers/EventsDiagramController.php
+++ src/Controllers/EventsDiagramController.php
@@ -5,6 +5,7 @@ namespace Mnikoei\EventsDiagram\Controllers;
 
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
 
 class EventsDiagramController extends Controller
 {
@@ -18,32 +19,77 @@ class EventsDiagramController extends Controller
         $eventListeners = $reflectionProperty->getValue(app('events'));
 
         $flatten = [];
+        $cnt= 1;
         foreach ($eventListeners as $event => $listeners){
             $arr[$event] = [];
             foreach ($listeners as $listener){
                 $flatten[$event][] = (new \ReflectionFunction($listener))->getStaticVariables()['listener'];
             }
+            $cnt++;
+            if ($cnt>1){break;
+            }
         }
 
-        $diagramData = $this->createDiagramJson($flatten);
-
-//        dd($diagramData);
+        $diagramData = $this->createDiagramData($flatten);
 
         return view('EventsDiagram::diagram', compact('diagramData'));
     }
 
-    public function createDiagramJson($events)
+    public function createDiagramData($events)
     {
-        $map = [];
-        foreach ($events as $event => $listeners){
+        $diagramData = [
+            'width' => 800,
+            'height' => 600,
+            'background' => "#eee",
+            'nodes' => [],
+            'links' => [],
+            'editable' => false
+        ];
 
-            $children = [];
+        foreach ($events as $event => $listeners){
+            $eventNode =  $this->createNode($event);
+            $diagramData['nodes'][] = $eventNode;
+
             foreach ($listeners as $listener){
-                $children['children'][] = ['text' => ['name' => $listener]];
+                $listenerNode = $this->createNode($listener);
+                $diagramData['nodes'][] = $listenerNode;
+
+                $link = $this->createLinkNode($listenerNode, $eventNode);
+                $diagramData['links'][] = $link;
             }
-            $map[] = array_merge(['text' => ['name' => $event]], $children);
         }
-        return json_encode($map, JSON_PRETTY_PRINT);
+        return $diagramData;
     }
 
+    public function createNode($event)
+    {
+        return [
+            'id' => Str::random(),
+            'content' => [
+                'text' => $event,
+                'color' => '#fab1a0'
+            ],
+            'width' => 150,
+            'height' => 160,
+            'point' => ['x' => rand(0, 1000), 'y' => rand(0, 1000)],
+            'shape' => 'rectangle',
+            'stroke' => 'black',
+            'strokeWeight' => 2
+        ];
+    }
+
+    public function createLinkNode($sourceNode, $destinationNode)
+    {
+        return [
+            'id' => Str::random(),
+            'source' => $sourceNode['id'],
+            'destination' => $destinationNode['id'],
+            'point'=> [
+                'x' => random_int(0, 1000),
+                'y' => 83.74688965089885
+             ],
+            'color' => '#74b9ff',
+            'pattern' => 'solid'
+        ];
+    }
 }
